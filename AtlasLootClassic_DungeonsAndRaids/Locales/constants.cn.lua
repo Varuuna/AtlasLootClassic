88dc3a784ff9074fadd654ac0ee036aea815daf3
Alex Roglic <95890164+arog85@users.noreply.github.com>
2024-02-01 17:50:06 -0500

    Localization update
    SoD localization update

--- AtlasLootClassic_DungeonsAndRaids/Locales/constants.cn.lua
+++ AtlasLootClassic_DungeonsAndRaids/Locales/constants.cn.lua
@@ -4,6 +4,13 @@ if not AL then return end
 
 -- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
 --@localization(locale="zhCN", format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="DungeonsAndRaids")@
+
+--Season of Discovery
+AL["SoD Raids"] = "探索赛季团队"
+AL["SoD Exclusives"] = "探索赛季独占"
+AL["SoD Exclusive: Phase 1"] = "探索赛季独占: 阶段1"
+AL["Lorgus Jett"] = "洛古斯·杰特"
+
 if AtlasLoot:GameVersion_LT(AtlasLoot.BC_VERSION_NUM) then return end
 --@localization(locale="zhCN", format="lua_additive_table", table-name="AL", handle-unlocalized="ignore", namespace="DungeonsAndRaidsTBC")@
 if AtlasLoot:GameVersion_LT(AtlasLoot.WRATH_VERSION_NUM) then return end
